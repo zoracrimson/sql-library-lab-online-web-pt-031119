fbc5ec67ad4c1c37584de8274a6a027a9de697b8
Kat Maldon <katharinamaldon@gmail.com>
2019-05-21 17:28:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,4 +42,3 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
-def select_series_title_with_most_human_characters
