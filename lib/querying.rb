ca5ac40642cc4b1259973e6d50492bb6a6ad7994
Kat Maldon <katharinamaldon@gmail.com>
2019-05-21 17:29:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,6 +8,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(*) DESC LIMIT 1;"
+end
 
 def select_name_and_series_subgenres_of_authors
    "SELECT authors.name, subgenres.name FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
